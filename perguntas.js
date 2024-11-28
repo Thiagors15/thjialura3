41078ab9340194058d07002c939631a7e576828c
Thiagors15 <thirodriguesgesualdo@gmail.com>
2024-11-28 11:07:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variavel em programação?',
+    'É um espaço nomeado na memória que armazena dados para uso do programa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o unico pais do mundo que possui tres capitais oficiais, e quais são essas capitais ?',
+    'O pais é a Africa do Sul. Suas tres capitais são: Pretória, Cidade do cabo, Bloemfontein'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um aloritmo é uma sequencia de passos lógicos para resolver um problema ou realizar uma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a diferença entre Who w Whom?',
+    'Whom é usado com sujeito, e Whom como objeto'
+)
